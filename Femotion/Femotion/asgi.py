40f74432e14b8ed0c37adef063ba45e212d94cb3
ewoo14 <101847661+ewoo14@users.noreply.github.com>
2023-10-09 16:25:30 +0900

    Update asgi.py

--- Femotion/Femotion/asgi.py
+++ Femotion/Femotion/asgi.py
@@ -1,13 +1,10 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import FacialEmotion.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Femotion.settings')
-
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
