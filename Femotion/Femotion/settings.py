85a06642e0ffc3b3d2d8bdda6d1f98b1462598f4
ewoo14 <101847661+ewoo14@users.noreply.github.com>
2023-10-09 16:13:35 +0900

    Update settings.py

--- Femotion/Femotion/settings.py
+++ Femotion/Femotion/settings.py
@@ -2,6 +2,8 @@
 
 import os
 from pathlib import Path
+import django 
+django.setup() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
