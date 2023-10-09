15d619b8da6d7d3db9a9b42f6103b396828e10ef
ewoo14 <101847661+ewoo14@users.noreply.github.com>
2023-10-09 16:09:58 +0900

    Update asgi.py

--- Femotion/Femotion/asgi.py
+++ Femotion/Femotion/asgi.py
@@ -1,4 +1,5 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
