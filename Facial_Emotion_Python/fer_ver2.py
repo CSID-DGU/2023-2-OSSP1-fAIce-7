563e215d5ea3143b7631f8b1f09ea78f51180013
ewoo14 <ewoo2821@gmail.com>
2023-11-01 11:54:30 +0900

    모델의 학습 방식 변경
    버전 1 : 3번의 학습, 가장 높은 accuracy 모델의 가중치만 출력
    버전 2 : 3번의 학습, 이전 단계의 학습 가중치로 다음 학습 진행하여 최종 가중치 출력
    공통 사항 : 매 학습마다 테스트 데이터셋을 변경하도록 적용

--- Facial_Emotion_Python/fer_ver2.py
+++ Facial_Emotion_Python/fer_ver2.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.optimizers import Adam
-from keras.models import Sequential, model_from_json, load_model
+from keras.models import Sequential, model_from_json
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization, Activation
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
 from sklearn import model_selection
 from math import ceil
+import os
 
 def preprocess_data():
     data = pd.read_csv('fer2013.csv')
@@ -28,7 +28,6 @@ def preprocess_data():
         X[i] = img_colored
 
     return X, y
-
 
 def clean_data_and_normalize(X, y):
     orig_class_names = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt',
@@ -52,16 +51,12 @@ def clean_data_and_normalize(X, y):
 
     return X, y
 
-
 def split_data(X, y):
     test_size = ceil(len(X) * 0.1)
-
-    # Split Data
     x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=test_size, random_state=42)
     x_train, x_val, y_train, y_val = model_selection.train_test_split(x_train, y_train, test_size=test_size,
                                                                       random_state=42)
     return x_train, y_train, x_val, y_val, x_test, y_test
-
 
 def data_augmentation(x_train):
     shift = 0.1
@@ -73,7 +68,6 @@ def data_augmentation(x_train):
     datagen.fit(x_train)
     return datagen
 
-
 def show_augmented_images(datagen, x_train, y_train):
     it = datagen.flow(x_train, y_train, batch_size=1)
     plt.figure(figsize=(10, 7))
@@ -83,9 +77,7 @@ def show_augmented_images(datagen, x_train, y_train):
         plt.yticks([])
         plt.grid(False)
         plt.imshow(it.next()[0][0], cmap='gray')
-        # plt.xlabel(class_names[y_train[i]])
     plt.show()
-
 
 def define_model(input_shape=(48, 48, 3), classes=7):
     num_features = 64
@@ -139,87 +131,58 @@ def define_model(input_shape=(48, 48, 3), classes=7):
 
     return model
 
-
 def plot_acc_loss(history):
-    # Plot accuracy graph
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label='val_accuracy')
     plt.xlabel('Epoch')
-    plt.ylabel('accuracy')
-    plt.ylim([0, 1.0])
+    plt.ylabel('Accuracy')
+    plt.ylim([0, 1])
     plt.legend(loc='upper left')
     plt.show()
 
-    # Plot loss graph
     plt.plot(history.history['loss'], label='loss')
     plt.plot(history.history['val_loss'], label='val_loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
-    # plt.ylim([0, 3.5])
     plt.legend(loc='upper right')
     plt.show()
 
-
 def save_model_and_weights(model, test_acc):
     test_acc = int(test_acc * 10000)
-    model_path = 'Saved-Models\\model' + str(test_acc) + '.h5'
-    model.save(model_path)
-    print(f'Model is saved at {model_path}.')
+    model_json = model.to_json()
+    with open('Saved-Models\\model' + str(test_acc) + '.json', 'w') as json_file:
+        json_file.write(model_json)
+    model.save_weights('Saved-Models\\model' + str(test_acc) + '.h5')
+    print('Model and weights are saved in separate files.')
 
+def load_model_and_weights(model_path, weights_path):
+    json_file = open(model_path, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    model.load_weights(weights_path)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    print('Model and weights are loaded and compiled.')
 
 fer_classes = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear']
 
-def run_model():
-
-    X, y = preprocess_data()
-    X, y = clean_data_and_normalize(X, y)
-    x_train, y_train, x_val, y_val, x_test, y_test = split_data(X, y)
-    datagen = data_augmentation(x_train)
-
-    epochs = 100
-    batch_size = 64
-
-    print("X_train shape:", x_train.shape)
-    print("Y_train shape:", y_train.shape)
-    print("X_test shape:", x_test.shape)
-    print("Y_test shape:", y_test.shape)
-    print("X_val shape:", x_val.shape)
-    print("Y_val shape:", y_val.shape)
-
-    model = define_model(input_shape=x_train[0].shape, classes=len(fer_classes))
-    model.summary()
-    model.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
-
-    checkpoint = ModelCheckpoint('best_model_run.h5', monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
-    history = model.fit(datagen.flow(x_train, y_train, batch_size=batch_size), epochs=epochs,
-                        steps_per_epoch=len(x_train) // batch_size,
-                        validation_data=(x_val, y_val), verbose=2, callbacks=[checkpoint])
-    test_loss, test_acc = model.evaluate(x_test, y_test, batch_size=batch_size)
-
-    plot_acc_loss(history)
-    save_model_and_weights(model, test_acc)
-
-
-run_model()
-
-
 def modified_split_data(X, y, random_seed):
     test_size = ceil(len(X) * 0.1)
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(
-        X, y, test_size=test_size, random_state=random_seed)
-    x_train, x_val, y_train, y_val = model_selection.train_test_split(
-        x_train, y_train, test_size=test_size, random_state=random_seed)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=test_size, random_state=random_seed)
+    x_train, x_val, y_train, y_val = model_selection.train_test_split(x_train, y_train, test_size=test_size, random_state=random_seed)
     return x_train, y_train, x_val, y_val, x_test, y_test
 
-
-def run_model_with_seed(X, y, random_seed):
+def run_model_and_save_weights(X, y, random_seed, save_name, model=None):
     x_train, y_train, x_val, y_val, x_test, y_test = modified_split_data(X, y, random_seed)
     
     datagen = data_augmentation(x_train)  # Using the data_augmentation function
     epochs = 100
     batch_size = 64
     
-    model = define_model(input_shape=x_train[0].shape, classes=len(fer_classes))
+    # 모델이 제공되지 않은 경우 새로운 모델을 정의
+    if model is None:
+        model = define_model(input_shape=x_train[0].shape, classes=len(fer_classes))
+    
     model.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
     
     history = model.fit(datagen.flow(x_train, y_train, batch_size=batch_size), epochs=epochs,
@@ -227,30 +190,28 @@ def run_model_with_seed(X, y, random_seed):
                         validation_data=(x_val, y_val), verbose=2)
     
     test_loss, test_acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+    
+    # 지정된 이름으로 모델의 가중치 저장
+    model.save_weights(save_name)
+    
     return history, test_acc, model
 
-
-def run_three_times_and_aggregate(X, y):
-    histories = []
-    test_accuracies = []
-    best_model_path = None
-    best_acc = 0
-
-    for seed in [42, 84]:
-        history, test_acc = run_model_with_seed(X, y, seed)
-        histories.append(history)
-        test_accuracies.append(test_acc)
-
-        # Check if this model has the best accuracy
-        if test_acc > best_acc:
-            best_acc = test_acc
-            best_model_path = 'best_model_run.h5'
-
-    # Load and save the best model
-    best_model = load_model(best_model_path)
-    save_model_and_weights(best_model, best_acc)
-
-    return histories, test_accuracies
+def run_iteratively_and_aggregate(X, y):
+    # 첫번째 학습된 모델
+    history1, test_acc1, model1 = run_model_and_save_weights(X, y, random_seed=42, save_name='first_weights.h5')
+    
+    # 두번째 학습된 모델
+    history2, test_acc2, model2 = run_model_and_save_weights(X, y, random_seed=84, save_name='second_weights.h5', model=model1)
+    if os.path.exists('first_weights.h5'):
+        os.remove('first_weights.h5')
+    
+    # 세번째 학습된 모델
+    model2.load_weights('second_weights.h5')
+    history3, test_acc3, model3 = run_model_and_save_weights(X, y, random_seed=126, save_name='final_serial_weights.h5', model=model2)
+    if os.path.exists('second_weights.h5'):
+        os.remove('second_weights.h5')
+    
+    return [history1, history2, history3], [test_acc1, test_acc2, test_acc3]
 
 
 def plot_aggregated_histories(histories):
@@ -285,9 +246,8 @@ def plot_aggregated_histories(histories):
     plt.tight_layout()
     plt.show()
 
-
 if __name__ == "__main__":
     X, y = preprocess_data()
     X, y = clean_data_and_normalize(X, y)
-    histories, test_accuracies = run_three_times_and_aggregate(X, y)
+    histories, test_accuracies = run_iteratively_and_aggregate(X, y)
     plot_aggregated_histories(histories)
