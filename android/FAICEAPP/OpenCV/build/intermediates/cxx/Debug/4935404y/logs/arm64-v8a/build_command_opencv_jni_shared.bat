9f784b6d11d11ee32cba617ac9d954211af4969d
ChanHyuk Park <PetterChanHyuk>
2023-11-13 18:32:12 +0900

    모든 파일 삭제

--- android/FAICEAPP/OpenCV/build/intermediates/cxx/Debug/4935404y/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\flinter\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\flinter\\Desktop\\ossproj\\2023-2-OSSP1-fAIce-7\\android\\FAICEAPP\\OpenCV\\.cxx\\Debug\\4935404y\\arm64-v8a" ^
-  opencv_jni_shared
