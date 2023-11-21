4da6c138e701865046ca4b942014e59a061e0ecc
ewoo14 <ewoo2821@gmail.com>
2023-11-21 15:54:12 +0900

    관심도 설정 로직 추가
    관심분야 카테고리를 백엔드에서 불러오며, 관심도를 사용자가 설정할 수 있도록 변경
    관심도는 총합이 반드시 10으로 고정됨

--- FRONTEND/cokkirri/src/routes/InterestSettingsPage.vue
+++ FRONTEND/cokkirri/src/routes/InterestSettingsPage.vue
@@ -1,30 +1,36 @@
-
 <template>
   <div class="interest-settings">
     <h2>관심분야 설정</h2>
-    <!-- 각 관심분야별 카테고리와 항목 설정 -->
-    <div v-for="i in 3" :key="i" class="interest-section">
+    <div v-for="(interest, index) in interests" :key="index" class="interest-section">
       <!-- 카테고리 선택 -->
       <div>
-        <label :for="'category' + i">카테고리 {{ i }}</label>
-        <select v-model="interests[i - 1].category" @change="updateItems(i)">
+        <label :for="'category' + index">카테고리 {{ index + 1 }}</label>
+        <select v-model="interest.category">
           <option disabled value="">카테고리</option>
-          <option v-for="(items, category) in categories" :key="category" :value="category">
-            {{ category }}
-          </option>
+          <!-- 백엔드에서 불러온 카테고리 옵션 -->
         </select>
       </div>
       <!-- 항목 선택 -->
       <div>
-        <label :for="'item' + i">항목 {{ i }}</label>
-        <select v-model="interests[i - 1].item">
+        <label :for="'item' + index">항목 {{ index + 1 }}</label>
+        <select v-model="interest.item">
           <option disabled value="">항목</option>
-          <option v-for="item in availableItems(i)" :key="item" :value="item">
+          <option v-for="item in availableItems(index)" :key="item" :value="item">
             {{ item }}
           </option>
         </select>
       </div>
+      <!-- 관심도 설정 슬라이더 및 점수 표시 -->
+      <div>
+        <label :for="'interest' + index">관심도 {{ index + 1 }}</label>
+        <input type="range" v-model="interest.score" min="1" max="10">
+        <span>{{ interest.score }}</span> <!-- 현재 관심도 점수 표시 -->
+      </div>
+      <!-- 삭제 버튼 -->
+      <button @click="removeInterest(index)">삭제</button>
     </div>
+    <!-- 항목 추가 버튼 -->
+    <button @click="addInterest">+</button>
     <button :disabled="!isComplete" @click="submitInterests">완료</button>
   </div>
 </template>
@@ -36,73 +42,110 @@ import { mapMutations } from 'vuex';
 export default {
   data() {
     return {
-      categories: {
-        '스포츠': ['축구', '농구', '야구', '당구'],
-        '게임': ['컴퓨터', '스위치', '보드게임', '오락실'],
-        '관람/감상': ['영화', '드라마', '뮤지컬', '전시회'],
-        '미용/패션': ['의류', '악세사리', '화장품', '네일'],
-        '애완동물': ['강아지', '고양이', '조류', '식물'],
-        '창작': ['그림', '음악', '사진', '글쓰기'],
-      },
-      interests: [{ category: '', item: '' }, { category: '', item: '' }, { category: '', item: '' }]
+      interests: [{ category: '', item: '', score: 10 }], // 초기 관심분야 데이터
+      categories: {} // 카테고리 데이터
     };
   },
+
+  created() {
+    this.fetchCategories(); // 컴포넌트 생성 시 카테고리 데이터 불러오기
+  },
+  
   computed: {
     isComplete() {
-      return this.interests.every(interest => interest.category && interest.item);
+      // 모든 관심분야가 적절히 설정되었는지 및 총점이 10점인지 확인
+      return this.interests.every(interest => interest.category && interest.item && interest.score) &&
+             this.interests.reduce((total, interest) => total + interest.score, 0) === 10;
     }
   },
+
   methods: {
-    updateItems(interestIndex) {
-      // 현재 선택된 카테고리에 따라 항목 목록 업데이트
-      this.interests[interestIndex - 1].item = '';
+    ...mapMutations(['setUserInterests']), // Vuex 뮤테이션 매핑
+
+    fetchCategories() {
+      // 백엔드에서 카테고리 데이터 불러오기
+      axios.get('/api/interest-categories')
+        .then(response => {
+          this.categories = response.data;
+        })
+        .catch(error => {
+          console.error('카테고리 불러오기 실패:', error);
+        });
     },
+
     availableItems(index) {
-    const currentCategory = this.interests[index - 1]?.category;
-    const currentItem = this.interests[index - 1]?.item;
-    const selectedItems = this.interests.map(i => i.item);
+      // 선택 가능한 항목 필터링
+      const selectedItems = this.interests
+        .filter((_, i) => i !== index)
+        .map(interest => interest.item);
 
-    if (currentCategory) {
-      return this.categories[currentCategory].filter(item => {
-        // 이미 선택된 항목은 선택이 불가하도록 하여 중복 방지
-        return item === currentItem || !selectedItems.includes(item);
-      });
-    } else {
-      return [];
-    }
-  },
-    submitInterests() {
-    // 관심분야 데이터를 백엔드로 전송
-    axios.post('/user/interests', {
-        id: this.$store.state.id,
-        category1: this.interests[0].category,
-        hobby1: this.interests[0].item,
-        category2: this.interests[1].category,
-        hobby2: this.interests[1].item,
-        category3: this.interests[2].category,
-        hobby3: this.interests[2].item
-      })
-      .then(response => {
-         console.log("서버 응답:", response.data); // 서버 응답 로깅
-        // Vuex 스토어에 관심분야 업데이트
-        console.log(this.interests);
-        this.$store.commit('setUserInterests', true);
-
-        // 성공 시 페이지 리디렉션
-        alert("관심분야 설정이 완료되었습니다.")
-        this.$router.push('/Starting'); // 예: Starting 페이지로 리디렉션
-      })
-      .catch(error => {
-        // 실패 시 경고 메시지 표시 후 로그인 화면으로 이동
-        console.error('오류:', error);
-        alert("설정이 완료되지 않아 서비스를 이용할 수 없습니다.");
-        this.$router.push('/');
-      });
+      return this.categories[this.interests[index].category]?.filter(item => !selectedItems.includes(item)) || [];
     },
-  ...mapMutations(['setUserInterests']) // Vuex 뮤테이션 매핑
+
+    addInterest() {
+      // 관심분야 추가
+      this.interests.push({ category: '', item: '', score: 1 });
+      this.redistributeScores();
+    },
+
+    removeInterest(index) {
+      // 관심분야 제거
+      const removedScore = this.interests[index].score;
+      this.interests.splice(index, 1);
+      if (this.interests.length > 0 && index > 0) {
+        this.interests[index - 1].score += removedScore;
+      }
+      this.redistributeScores();
+    },
+
+    redistributeScores() {
+      // 관심도 총합 10점 유지
+      let totalScore = this.interests.reduce((total, interest) => total + interest.score, 0);
+      let excess = totalScore - 10;
+
+      if (excess !== 0) {
+        for (let i = 0; i < this.interests.length; i++) {
+          if (excess > 0) {
+            let deduction = Math.min(this.interests[i].score - 1, excess);
+            this.interests[i].score -= deduction;
+            excess -= deduction;
+          } else {
+            let addition = Math.min(1 - this.interests[i].score, -excess);
+            this.interests[i].score += addition;
+            excess += addition;
+          }
+
+          if (excess === 0) break;
+        }
+      }
+    },
+
+    submitInterests() {
+      // 관심분야 데이터 백엔드로 전송
+      axios.post('/user/interests', {
+          id: this.$store.state.id,
+          interests: this.interests.map(interest => ({
+            category: interest.category,
+            item: interest.item,
+            score: interest.score
+          }))
+        })
+        .then(response => {
+          console.log("서버 응답:", response.data);
+          this.$store.commit('setUserInterests', true);
+          alert("관심분야 설정이 완료되었습니다.")
+          this.$router.push('/Starting');
+        })
+        .catch(error => {
+          console.error('오류:', error);
+          alert("설정이 완료되지 않아 서비스를 이용할 수 없습니다.");
+          this.$router.push('/');
+        });
+    },
   }
 };
 </script>
+
 
 <style lang="scss" scoped>
 @import "../scss/main";
