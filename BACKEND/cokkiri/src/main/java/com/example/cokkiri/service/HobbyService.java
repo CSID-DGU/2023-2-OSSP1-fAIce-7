87d5b94fcd21cf5bc5017160b4ad847505f0dd51
ewoo14 <ewoo2821@gmail.com>
2023-11-25 18:07:02 +0900

    Update HobbyService.java

--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/service/HobbyService.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/service/HobbyService.java
@@ -18,8 +18,36 @@ public class HobbyService {
         return hobbyRepository.findByUser_Id(userId);
     }
 
+<<<<<<< HEAD
     public Hobby saveHobby(Hobby hobby) {
         // 취미 저장
         return hobbyRepository.save(hobby);
+=======
+    public void saveUserHobbies(UserHobbiesDTO userHobbiesDTO) {
+        // 유저 ID로 User 객체를 찾거나 새로 생성
+        User user = userRepository.findById(userHobbiesDTO.getId())
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setId(userHobbiesDTO.getId());
+                    return userRepository.save(newUser); // 새로 생성된 User 저장
+                });
+
+        // 취미 정보 생성 및 저장
+        Hobby hobby = new Hobby();
+        hobby.setId(user.getId()); // User ID 설정
+
+        Map<String, Integer> hobbyScores = userHobbiesDTO.getInterests().stream()
+                .collect(Collectors.toMap(HobbyDTO::getItem, HobbyDTO::getScore));
+        logger.info(userHobbiesDTO.toString());
+
+        for (int i = 0; i < HobbyUtils.HOBBIES.size(); i++) {
+            String hobbyName = HobbyUtils.HOBBIES.get(i);
+            int score = hobbyScores.getOrDefault(hobbyName, 0);
+            hobby.setHobby(i + 1, score);
+            logger.info("Hobby: {}, Score: {}", hobbyName, score);
+        }
+
+        hobbyRepository.save(hobby);
+>>>>>>> parent of dfb01f6b (버그 수정)
     }
 }
