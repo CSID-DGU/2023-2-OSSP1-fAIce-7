97e8ad7385f988e81b98275d5d91d23c9f4bffb5
jhan0121 <jhan0121@gmail.com>
2023-12-05 16:31:03 +0900

    db 수정

--- BACKEND/cokkiri/src/main/java/com/example/cokkiri/model/User.java
+++ BACKEND/cokkiri/src/main/java/com/example/cokkiri/model/User.java
@@ -42,30 +42,6 @@ public class User {
     @Column
     private String studentNum;
 
-    // 취미 1의 카테고리
-    @Column
-    private String category1;
-
-    // 취미 1
-    @Column
-    private String hobby1;
-
-    // 취미 2의 카테고리
-    @Column
-    private String category2;
-
-    // 취미 2
-    @Column
-    private String hobby2;
-
-    // 취미 3의 카테고리
-    @Column
-    private String category3;
-
-    // 취미 3
-    @Column
-    private String hobby3;
-
     //수업 제한날짜
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime restrctionDate;
